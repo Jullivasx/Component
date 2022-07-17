f9ce90ae29e4229e0ffc7d33983a60932abef0df
Jullivasx <jullivasx@gmail.com>
2022-07-17 07:12:35 +0300

    Add method append, remove, addClass and other

---
+++ Component.js
@@ -0,0 +1,70 @@
+class Component {
+    /**
+     * Создание комнопента
+     * @param {Object} param
+     *      {
+     *          tag:string - тип элемента по-умолчанию 'div'
+     *          classes:[] - список класов
+     *          attrs:{} - список атрибутов
+     *      }
+     */
+    constructor({tag='div', classes=[], attrs={}} = {}) {
+        this.element = document.createElement(tag);
+        this.addClasses(classes);
+    }
+
+    addClasses(classes = []) {
+        if (typeof(classes) === 'string') {
+            this.addClass(classes);
+            return this;
+        }
+        classes.forEach(_class => this.addClass(_class))
+        return this;
+    }
+
+    addClass(_class) {
+        this.element.classList.add(_class);
+        return this;
+    }
+
+    appendElement(element) {
+        this.element.appendChild(element);
+        return this;
+    }
+
+    appendToElement(element) {
+        element.appendChild(this.element);
+        return this;
+    }
+
+    append(component) {
+        this.appendElement(component.element);
+        return this;
+    }
+
+    appendTo(component) {
+        component.appendElement(this.element);
+        return this;
+    }
+
+    removeElement(element) {
+        this.element.removeChild(element);
+        return this;
+    }
+
+    removeToElement(element) {
+        element.removeChild(this.element);
+        return this;
+    }
+
+    remove(component) {
+        this.removeElement(component.element);
+        return this;
+    }
+
+    removeTo(component) {
+        component.removeElement(this.element);
+        return this;
+    }
+
+}
